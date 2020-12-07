640f5acab706b3e57e7d09a7a57d3835874dea76
Brent Goubert <brent.goubert@student.hogent.be>
2020-12-07 23:47:23 +0100

    Added domain

---
+++ TravelAppBackend/Models/ItemLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelAppBackend.Models
+{
+    public class ItemLine
+    {
+        public int Id { get; set; }
+        public Item Item { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ TravelAppBackend/Models/Journey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelAppBackend.Models
+{
+    public class Journey
+    {
+        public int Id { get; set; }
+        public DateTime Start { get; set; }
+        public List<ItemLine> Items { get; set; }
+        public User User { get; set; }
+    }
+}
